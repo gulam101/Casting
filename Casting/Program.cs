2d1a9acfa5ce448b0181b2542cf3d559249210ab
unknown <gfram@hotmail.com>
2019-09-22 14:38:54 +0100

    first commit

---
+++ Casting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short num1 = 10;
+            short num2 = 20;
+            short sum = (short)(num1 + num2);
+            int num01 = 100;
+            double d01 = num01;
+
+            int num = (int)(d01 + num01);
+
+            int a = 10;
+            object o = a; //boxing
+            int b = (int)o; //unboxing
+
+            Console.WriteLine(b);
+
+            Console.WriteLine(num);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
